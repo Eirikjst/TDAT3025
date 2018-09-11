0eff1a45927879b6222b54fb8d2a9d3daa3c65d0
eirikjst <eirikjst@ntnu.stud.no>
2018-09-11 11:36:16 +0200

    added output from one run with 20 steps

--- exercise2_part1/MNIST_Fashion.py
+++ exercise2_part1/MNIST_Fashion.py
@@ -75,10 +75,35 @@ session = tf.Session()
 # Initialize tf.Variable objects
 session.run(tf.global_variables_initializer())
 
-for epoch in range(200):
+for epoch in range(20):
     for batch in range(batches):
         session.run(minimize_operation, {model.x: x_train_batches[batch], model.y: y_train_batches[batch]})
     #if epoch % 10 == 0:
     print("epoch", epoch,", accuracy", session.run(model.accuracy, {model.x: x_test, model.y: y_test}))
 
 session.close()
+
+"""
+output:
+
+epoch 0 , accuracy 0.84020007
+epoch 1 , accuracy 0.83770007
+epoch 2 , accuracy 0.86300004
+epoch 3 , accuracy 0.83610004
+epoch 4 , accuracy 0.85240006
+epoch 5 , accuracy 0.84800005
+epoch 6 , accuracy 0.84470004
+epoch 7 , accuracy 0.86620003
+epoch 8 , accuracy 0.84900004
+epoch 9 , accuracy 0.81490004
+epoch 10 , accuracy 0.83210003
+epoch 11 , accuracy 0.83800006
+epoch 12 , accuracy 0.83940005
+epoch 13 , accuracy 0.8564
+epoch 14 , accuracy 0.8686001
+epoch 15 , accuracy 0.8579
+epoch 16 , accuracy 0.8503
+epoch 17 , accuracy 0.85420007
+epoch 18 , accuracy 0.83540004
+epoch 19 , accuracy 0.84920007
+"""
